346535ca8eb6dc20ac591697499d046515207223
liguoxiang <616930780@qq.com>
2022-08-16 07:17:47 +0800

    08.16

---
+++ src/main/java/Daily/year2022/M8/D16/q1656.java
@@ -0,0 +1,36 @@
+package Daily.year2022.M8.D16;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class q1656 {
+}
+
+class OrderedStream {
+    String[] store;
+    int ptr;
+    int n;
+    public OrderedStream(int n) {
+        store = new String[n + 1];
+        ptr =  1;
+        this.n = n;
+    }
+
+    public List<String> insert(int idKey, String value) {
+        store[idKey] = value;
+
+        List<String> ans = new ArrayList<>();
+        if(store[ptr] == null){
+            return ans;
+        }
+
+        while(ptr <= n && store[ptr] != null){
+            ans.add(store[ptr]);
+            ptr++;
+        }
+
+        return ans;
+
+
+    }
+}
