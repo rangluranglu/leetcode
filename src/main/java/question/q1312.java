37d12a067fa4a8d32eeb82acf4cdf5084ee7978b
liguoxiang <616930780@qq.com>
2022-08-25 22:42:25 +0800

    1312.让字符串成为回文的最少插入次数

--- src/main/java/question/q1312.java
+++ src/main/java/question/q1312.java
@@ -1,4 +1,24 @@
 package question;
 
 public class q1312 {
+    public int minInsertions(String s) {
+        int n = s.length();
+        int[][] dp = new int[n][n];
+
+        for (int i = n - 1; i >= 0; i--) {
+            dp[i][i] = 1;
+            char c1 = s.charAt(i);
+
+            for(int j = i + 1; j < n; j++){
+                char c2 = s.charAt(j);
+                if(c1 == c2){
+                    dp[i][j] = dp[i + 1][j - 1] + 2;
+                }else{
+                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        return n - dp[0][n - 1];
+    }
 }
