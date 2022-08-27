462591bfa52335e379bfcf2eacbcdf9921bfd94b
liguoxiang <616930780@qq.com>
2022-08-27 16:38:46 +0800

    08.27

---
+++ src/main/java/Daily/year2022/M8/D27/q662.java
@@ -0,0 +1,26 @@
+package Daily.year2022.M8.D27;
+
+import Common.TreeNode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class q662 {
+    Map<Integer, Integer> levelMin = new HashMap<>();
+    public int widthOfBinaryTree(TreeNode root) {
+        // bfs 简单
+        // dfs 呢？
+        return dfs(root, 1, 1);
+
+    }
+
+
+    public int dfs(TreeNode node, int depth, int index){
+        if(node == null){
+            return 0;
+        }
+
+        levelMin.putIfAbsent(depth, index);
+        return Math.max(index - levelMin.get(depth) + 1, Math.max(dfs(node.left, depth + 1, index * 2), dfs(node.right, depth + 1, index * 2 + 1)));
+    }
+}
