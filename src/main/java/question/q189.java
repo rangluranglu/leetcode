77ebd412f814f7d94f8dbf4e5ff758e572497616
liguoxiang <liguoxiang@xiaomi.com>
2022-08-30 17:12:51 +0800

    189.轮转数组

---
+++ src/main/java/question/q189.java
@@ -0,0 +1,23 @@
+package question;
+
+public class q189 {
+    public void rotate(int[] nums, int k) {
+        // reverse
+        int n = nums.length;
+        k %= n;
+        reverse(nums, 0, n - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, n - 1);
+
+    }
+
+    public void reverse(int[] nums, int start, int end){
+        while(start < end){
+            int tmp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = tmp;
+            start += 1;
+            end -= 1;
+        }
+    }
+}
