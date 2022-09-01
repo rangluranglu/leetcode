a7a27831ea1062691a8bda1cb350b9caa2a665fe
liguoxiang <liguoxiang@xiaomi.com>
2022-09-01 11:49:48 +0800

    09.01

---
+++ src/main/java/Daily/year2022/M9/D1/q1475.java
@@ -0,0 +1,22 @@
+package Daily.year2022.M9.D1;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class q1475 {
+    public int[] finalPrices(int[] prices) {
+        int n = prices.length;
+        int[] ans = new int[n];
+
+        Deque<Integer> stack = new ArrayDeque<>();
+        for (int i = n - 1; i >= 0; i--) {
+            while(!stack.isEmpty() && stack.peek() > prices[i]){
+                stack.pop();
+            }
+
+            ans[i] = stack.isEmpty()? prices[i] : prices[i] - stack.peek();
+            stack.push(prices[i]);
+        }
+        return ans;
+    }
+}
