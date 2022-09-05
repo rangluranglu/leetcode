7680982b3859f226e5196e13d0db513a1e38d75b
liguoxiang <liguoxiang@xiaomi.com>
2022-09-05 19:30:33 +0800

    80.删除有序数组中的重复项II

---
+++ src/main/java/question/q80.java
@@ -0,0 +1,22 @@
+package question;
+
+public class q80 {
+    public int removeDuplicates(int[] nums) {
+        int n = nums.length;
+
+        if(n <= 2){
+            return n;
+        }
+
+        int slow = 2, fast = 2;
+        while(fast < n){
+            if(nums[slow - 2] != nums[fast]){
+                nums[slow] = nums[fast];
+                ++slow;
+            }
+            fast++;
+        }
+
+        return slow;
+    }
+}
