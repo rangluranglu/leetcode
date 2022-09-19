39ca49c9aac82087c762ead0c11b1acf3d7e78cd
liguoxiang <liguoxiang@xiaomi.com>
2022-09-19 14:57:07 +0800

    第87场双周赛

---
+++ src/main/java/WeekCompetition/year2022/DW87/q2409.java
@@ -0,0 +1,24 @@
+package WeekCompetition.year2022.DW87;
+
+import java.util.Arrays;
+
+public class q2409 {
+    int[] months = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    public int countDaysTogether(String arriveAlice, String leaveAlice, String arriveBob, String leaveBob) {
+        String[] start = arriveAlice.compareTo(arriveBob) < 0 ? arriveBob.split("-") : arriveAlice.split("-");
+        String[] end = leaveAlice.compareTo(leaveBob) < 0 ?  leaveAlice.split("-") : leaveBob.split("-");
+        int day1 = count(Integer.valueOf(start[0]), Integer.valueOf(start[1]));
+        int day2 = count(Integer.valueOf(end[0]), Integer.valueOf(end[1]));
+        return day2 - day1 + 1 < 0 ? 0 : day2 - day1 + 1;
+    }
+
+    private int count(Integer month, Integer day) {
+        int ans = 0;
+        for(int i = 1; i < month; i++){
+            ans += months[i];
+        }
+        ans += day;
+
+        return ans;
+    }
+}
---
+++ src/main/java/WeekCompetition/year2022/DW87/q2412.java
@@ -0,0 +1,22 @@
+package WeekCompetition.year2022.DW87;
+
+public class q2412 {
+    public long minimumMoney(int[][] transactions) {
+        // 贪心
+        // 考虑最坏的情况， 先亏钱再赚钱
+        // transactions = [cost, cashback]
+
+        // 对于cost_i <= cashback_i 的交易，这笔交易可以发生在亏钱后，此时初始 money = totalLose + cost_i;
+        // 对于cost_i > cashback_i 的交易，这笔交易可以发生在最后一笔亏钱时，由于已经计入totalLose 中，需要从 totalLose 中减去
+        // cost_i - cashback_i，再加上cost_i 得到 money = totalLost + cashback_i;
+        long totalLose = 0L;
+        long mx = 0;
+
+        for(int[] t : transactions){
+            totalLose += Math.max(t[0] - t[1], 0);
+            mx = Math.max(mx, Math.min(t[0], t[1]));
+        }
+
+        return totalLose + mx;
+    }
+}
