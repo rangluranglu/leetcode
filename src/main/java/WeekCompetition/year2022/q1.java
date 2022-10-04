ef9b6dbf8ef5c99035a7a8c2a32229f938c52054
liguoxiang <616930780@qq.com>
2022-10-04 11:19:04 +0800

    10.02

---
+++ src/main/java/WeekCompetition/year2022/q1.java
@@ -0,0 +1,20 @@
+package WeekCompetition.year2022;
+
+public class q1 {
+}
+
+
+class LUPrefix {
+
+    public LUPrefix(int n) {
+
+    }
+
+    public void upload(int video) {
+
+    }
+
+    public int longest() {
+
+    }
+}
