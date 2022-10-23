649fda1283e99bf333679324fb1d7a5cd47e1413
liguoxiang <616930780@qq.com>
2022-10-23 19:16:19 +0800

    10.23

--- src/main/java/WeekCompetition/year2022/atemplate/question2.java
+++ src/main/java/WeekCompetition/year2022/atemplate/question2.java
@@ -1,4 +1,44 @@
 package WeekCompetition.year2022.atemplate;
 
 public class question2 {
+    public int subarrayGCD(int[] nums, int k) {
+
+        // 最大公约数
+        int n = nums.length;
+        int ans = 0;
+
+        for(int i = 0; i < n; i++){
+            int tmp = nums[i];
+
+            for(int j = i; j < n; j++){
+                tmp = findGCD(tmp, nums[j]);
+                if(tmp == k){
+                    ans += 1;
+                }else if(tmp % k != 0){
+                    break;
+                }
+            }
+        }
+
+        return ans;
+
+    }
+    
+    public int findGCD(int a, int b){
+        if(b > a){
+            return findGCD(b, a);
+        }
+
+        // a >= b;
+        int c = 0;
+
+        while(a % b != 0){
+            c = a % b;
+            a = b;
+            b = c;
+        }
+
+        return c;
+
+    }
 }
--- src/main/java/WeekCompetition/year2022/atemplate/question3.java
+++ src/main/java/WeekCompetition/year2022/atemplate/question3.java
@@ -1,4 +1,7 @@
 package WeekCompetition.year2022.atemplate;
 
 public class question3 {
+    public long minCost(int[] nums, int[] cost) {
+
+    }
 }
