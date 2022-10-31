de6689e16b9a44d92b159d28b28e764eaa5ff353
liguoxiang <liguoxiang@xiaomi.com>
2022-10-31 17:40:05 +0800

    10.31

---
+++ src/main/java/Daily/year2022/M10/D31/q481.java
@@ -0,0 +1,37 @@
+package Daily.year2022.M10.D31;
+
+public class q481 {
+    public int magicalString(int n) {
+        if(n < 4){
+            return 1;
+        }
+
+        char[] s = new char[n];
+        s[0] = '1';
+        s[1] = '2';
+        s[2] = '2';
+        int res = 1;
+        int i = 2, j = 3;
+
+        while (j < n){
+            // 需要的长度
+            int length = s[i] - '0';
+            // 下一个数字
+            int num = 3 - (s[j - 1] - '0');
+            // 填充数字
+            while (j < n && length > 0){
+                s[j] = (char) ('0' + num);
+                if(num == 1){
+                    res ++;
+                }
+
+                j ++;
+                length --;
+            }
+            i++;
+        }
+
+        return res;
+
+    }
+}
