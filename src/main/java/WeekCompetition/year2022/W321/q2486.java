7a76bbdeaf62cbe94f8b2df56a5fc77b1070fba2
李国祥[liguoxiang] <p-liguoxiang1@xiaomi.com>
2022-12-05 15:33:54 +0800

    第321场周赛

---
+++ src/main/java/WeekCompetition/year2022/W321/q2486.java
@@ -0,0 +1,23 @@
+package WeekCompetition.year2022.W321;
+
+public class q2486 {
+    public int appendCharacters(String s, String t) {
+        int i = 0, j = 0;
+        int m = s.length(), n = t.length();
+
+        while (i < m && j < n){
+            if(s.charAt(i) ==t.charAt(j)){
+                i ++;
+                j ++;
+            }else {
+                i ++;
+            }
+        }
+
+        if(j == n){
+            return 0;
+        }else {
+            return n - j;
+        }
+    }
+}
