227a3df25bca36634bd06d13afde5c90164c43e5
李国祥[liguoxiang] <p-liguoxiang1@xiaomi.com>
2022-12-18 18:29:19 +0800

    12.18

---
+++ src/main/java/Daily/year2022/M12/D18/q1703.java
@@ -0,0 +1,26 @@
+package Daily.year2022.M12.D18;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class q1703 {
+    public int minMoves(int[] nums, int k) {
+
+                List<Integer> g = new ArrayList<Integer>();
+                List<Integer> preSum = new ArrayList<Integer>();
+                preSum.add(0);
+                for (int i = 0; i < nums.length; i++) {
+                    if (nums[i] == 1) {
+                        g.add(i - g.size());
+                        preSum.add(preSum.get(preSum.size() - 1) + g.get(g.size() - 1));
+                    }
+                }
+                int m = g.size(), res = Integer.MAX_VALUE;
+                for (int i = 0; i <= m - k; i++) {
+                    int mid = i + k / 2;
+                    int r = g.get(mid);
+                    res = Math.min(res, (1 - k % 2) * r + (preSum.get(i + k) - preSum.get(mid + 1)) - (preSum.get(mid) - preSum.get(i)));
+                }
+                return res;
+    }
+}
