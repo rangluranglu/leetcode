ee60017d128f7ecc7ef0e24991642e6299a0ea33
李国祥[liguoxiang] <p-liguoxiang1@xiaomi.com>
2022-12-22 16:31:14 +0800

    第91场双周赛

---
+++ src/main/java/WeekCompetition/year2022/DW91/q2465.java
@@ -0,0 +1,23 @@
+package WeekCompetition.year2022.DW91;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class q2465 {
+    public int distinctAverages(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        Set<Double> set = new HashSet<>();
+        for(int i = 0; i <= n / 2; i++){
+            int a = nums[i];
+            int b = nums[n - 1 - i];
+
+
+            set.add(((double )(a+b)) / 2);
+
+        }
+
+        return set.size();
+    }
+}
