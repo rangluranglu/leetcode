9f5d23c72df54695aa0eb8f0cebb6415465b7d94
李国祥[liguoxiang] <p-liguoxiang1@xiaomi.com>
2022-12-27 09:43:03 +0800

    12.27

---
+++ src/main/java/Daily/year2022/M12/D27/q2027.java
@@ -0,0 +1,20 @@
+package Daily.year2022.M12.D27;
+
+public class q2027 {
+    public int minimumMoves(String s) {
+            int ans = 0, index = 0;
+
+            while (index < s.length()){
+                while (index<s.length() && s.charAt(index) == 'O'){
+                    index ++;
+                }
+                if(index < s.length()){
+                    ans += 1;
+                    index += 3;
+                }
+
+            }
+
+            return ans;
+    }
+}
