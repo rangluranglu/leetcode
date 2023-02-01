eb6bbbb4ed1357ed7e635e263df0eeec367e0a3f
李国祥[liguoxiang] <p-liguoxiang1@xiaomi.com>
2023-02-01 10:25:34 +0800

    02.01

---
+++ src/main/java/Year2023/Daily/M2/D1/q2325.java
@@ -0,0 +1,34 @@
+package Year2023.Daily.M2.D1;
+
+import jdk.internal.org.objectweb.asm.Handle;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class q2325 {
+    public String decodeMessage(String key, String message) {
+        // 制造对照表
+
+        Map<Character, Character> map = new HashMap<>();
+        char value = 'a';
+        for(char c : key.toCharArray()){
+            if(c != ' ' && !map.containsKey(c)){
+                map.put(c, value);
+                value ++;
+            }
+
+
+            if(value > 'z'){
+                break;
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(char c : message.toCharArray()){
+            sb.append(c == ' ' ? c : map.get(c));
+        }
+
+        return sb.toString();
+
+    }
+}
