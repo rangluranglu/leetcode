2112611f600deb22eb406b4e999d9cbc57de15fb
李国祥[liguoxiang] <p-liguoxiang1@xiaomi.com>
2023-02-06 01:05:35 +0800

    02.04

---
+++ src/main/java/Year2023/Daily/M2/D4/q1798.java
@@ -0,0 +1,17 @@
+package Year2023.Daily.M2.D4;
+
+import java.util.Arrays;
+
+public class q1798 {
+    public int getMaximumConsecutive(int[] coins) {
+        int res = 1;
+        Arrays.sort(coins);
+        for (int i : coins) {
+            if (i > res) {
+                break;
+            }
+            res += i;
+        }
+        return res;
+    }
+}
