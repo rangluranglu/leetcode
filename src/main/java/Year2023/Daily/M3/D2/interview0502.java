b476f182dd08c9935f365f9042791fe5e546b403
李国祥[liguoxiang] <p-liguoxiang1@xiaomi.com>
2023-03-02 19:17:03 +0800

    03.02

--- src/main/java/Year2023/Daily/M3/D2/interview0502.java
+++ src/main/java/Year2023/Daily/M3/D2/interview0502.java
@@ -4,7 +4,7 @@ public class interview0502 {
     public String printBin(double num) {
         // 小数的二进制转换 0 到 1
 
-        StringBuilder sb = new StringBuilder();
+        StringBuilder sb = new StringBuilder("0.");
         while (sb.length() <= 32 && num != 0){
             num *= 2;
             int digit = (int) num;
