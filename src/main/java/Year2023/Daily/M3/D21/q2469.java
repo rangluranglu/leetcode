21e9ba9020375a136f77c0eb893c3bcc104a0199
李国祥[liguoxiang] <p-liguoxiang1@xiaomi.com>
2023-03-21 20:45:48 +0800

    03.21

---
+++ src/main/java/Year2023/Daily/M3/D21/q2469.java
@@ -0,0 +1,7 @@
+package Year2023.Daily.M3.D21;
+
+public class q2469 {
+    public double[] convertTemperature(double celsius) {
+        return new double[] {celsius + 273.15, celsius * 1.80 + 32.00};
+    }
+}
