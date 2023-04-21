bc576e1b8f799da3b20f21422cdb7f3ff137e9ce
李国祥[liguoxiang] <p-liguoxiang1@xiaomi.com>
2023-04-21 14:40:21 +0800

    04.21

---
+++ src/main/java/Year2023/Daily/M4/D21/q2413.java
@@ -0,0 +1,10 @@
+package Year2023.Daily.M4.D21;
+
+public class q2413 {
+    public int smallestEvenMultiple(int n) {
+        if(n % 2 == 0){
+            return n;
+        }
+        return 2 * n;
+    }
+}
