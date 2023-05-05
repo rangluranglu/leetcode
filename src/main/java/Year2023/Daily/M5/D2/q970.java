47532b96d8a4cf316c41ded1c6c32ac06a307af3
李国祥[liguoxiang] <p-liguoxiang1@xiaomi.com>
2023-05-05 10:39:53 +0800

    05.02 970.强整数

--- src/main/java/Year2023/Daily/M5/D2/q970.java
+++ src/main/java/Year2023/Daily/M5/D2/q970.java
@@ -1,4 +1,37 @@
 package Year2023.Daily.M5.D2;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class q970 {
+
+    public List<Integer> powerfulIntegers(int x, int y, int bound) {
+        // bound <= 10^6
+
+        Set<Integer> set = new HashSet<>();
+        int value1 = 1;
+
+        for(int i = 0; i < 21; i++){
+            int  value2 = 1;
+            for(int j = 0; j < 21; j++){
+                int value = value1 + value2;
+                if(value <= bound){
+                    set.add(value);
+                }else {
+                    break;
+                }
+
+                value2 *= y;
+            }
+
+            if(value1 > bound){
+                break;
+            }
+            value1 *= x;
+        }
+
+        return new ArrayList<>(set);
+    }
 }
