92e8be39f91f733ef837c12257358bf60fd1aefd
李国祥[liguoxiang] <p-liguoxiang1@xiaomi.com>
2023-07-02 16:18:18 +0800

    参加第352场周赛

--- src/main/java/Year2023/Week/atemplate/q3.java
+++ src/main/java/Year2023/Week/atemplate/q3.java
@@ -9,16 +9,17 @@ public class q3 {
         // 子数组相差不大于2，也就是子数组最多有两个数字
         // 滑动窗口
 
-        int left = 0, n = nums.length;
-        int ans = 0;
+        int left = 0, n = nums.length, right = 0;
+        long ans = 0;
         int lastRight = 0;
 
+        // 双单调队列
         // 单调队列
         // 递减
         Deque<Integer> queue1 = new ArrayDeque<>();
         // 递增
         Deque<Integer> queue2 = new ArrayDeque<>();
-        for(int right = 0; right < n; right ++){
+        while (right < n){
             // stack1 递增栈 -- 从栈顶往栈底看，是单调递增的关系 -- 最小
             while (!queue1.isEmpty() && nums[queue1.peekFirst()] < nums[right]){
                 queue1.pollFirst();
@@ -31,28 +32,22 @@ public class q3 {
             }
             queue2.offerFirst(right);
 
-            if(nums[queue1.peekLast()] - nums[queue2.peekLast()] > 2){
+            while (nums[queue1.peekLast()] - nums[queue2.peekLast()] > 2){
                 //如何更新最大值，最小值
                 //System.out.println("left" + left + "right" + right);
-                ans += (right - left) * (right - left + 1) / 2;
-                if(left <= lastRight){
-                    ans -= (lastRight - left) * (lastRight - left + 1) / 2;
-                }
-                lastRight = right;
                 left ++;
-
                 while (!queue1.isEmpty() && queue1.peekLast() < left){
                     queue1.pollLast();
                 }
-
                 while (!queue2.isEmpty() && queue2.peekLast() < left){
                     queue2.pollLast();
                 }
             }
+
+            right ++;
+            ans += (right - left);
+
         }
-        ans += (n - left) * (n - left + 1) / 2;
-
-
 
         return ans;
     }
--- src/main/java/Year2023/Week/atemplate/q4.java
+++ src/main/java/Year2023/Week/atemplate/q4.java
@@ -1,5 +1,33 @@
 package Year2023.Week.atemplate;
 
+import java.util.Arrays;
+
 public class q4 {
+    public int sumImbalanceNumbers(int[] nums) {
+        int ans = 0, n = nums.length;
+        boolean[] vis = new boolean[n + 2];
+
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(vis, false);
+
+            vis[nums[i]] = true;
+            int cnt = 0;
+            for (int j = i + 1; j < n; j++) {
+                int x = nums[j];
+
+                if(!vis[x]){
+                    cnt ++;
+                    if(vis[x - 1]) cnt --;
+                    if(vis[x + 1]) cnt --;
+
+                    vis[x] = true;
+                }
+                ans += cnt;
+            }
+        }
+
+        return ans;
+
+    }
 
 }
