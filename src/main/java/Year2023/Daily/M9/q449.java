5efcb32d96ff83cca73d93ea4d459a8fe5d713d5
李国祥[liguoxiang] <p-liguoxiang1@xiaomi.com>
2023-09-04 16:10:09 +0800

    09.04 449.序列化和反序列化二叉搜索树

---
+++ src/main/java/Year2023/Daily/M9/q449.java
@@ -0,0 +1,66 @@
+package Year2023.Daily.M9;
+
+import Common.TreeNode;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+public class q449 {
+}
+
+class Codec {
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        // 序列化
+        // 二叉搜索树 - 后序遍历
+
+        List<Integer> list = new ArrayList<>();
+        postOrder(root, list);
+        String str = list.toString();
+        return str.substring(1, str.length() - 1);
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        // 反序列化
+        if(data.isEmpty()){
+            return null;
+        }
+
+        String[] arr = data.split(", ");
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        int length = arr.length;
+        for (int i = 0; i < length; i++) {
+            stack.push(Integer.parseInt(arr[i]));
+        }
+
+        return construct(Integer.MIN_VALUE, Integer.MAX_VALUE, stack);
+    }
+
+    private TreeNode construct(int lower, int upper, Deque<Integer> stack){
+        if(stack.isEmpty() || stack.peek() < lower || stack.peek() > upper){
+            return null;
+        }
+
+        int val = stack.pop();
+        TreeNode root = new TreeNode(val);
+        root.right = construct(val, upper, stack);
+        root.left = construct(lower, val, stack);
+        return root;
+    }
+
+    private void postOrder(TreeNode root, List<Integer> list){
+        if(root == null){
+            return;
+        }
+        postOrder(root.left, list);
+        postOrder(root.right, list);
+
+        list.add(root.val);
+    }
+
+
+}
